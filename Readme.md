933ed211320f262a60bbcc0d3cc90587d4275c25
vilrodriguez <vilmango@hotmail.com>
2019-12-26 11:22:50 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+ -- ACR Payment Frequency App --
+
+ This is a small application for a Car Loan company customer service representatives. It will help calculate the customer´s payments from 1 frequency to a different one; for example: from biweekly payments to monthly. The customer service representative usually does this manually and takes some minutes to calculate, adding a window for errors since it´s done while talking to the customer on the phone; this apps solves this problem using the correct amounts and helping the representative to focus on the conversation withthe customer instead of the numbers.
+
+ On progress and for practice.
---
+++ app.js
@@ -0,0 +1,33 @@
+// import { numero } from '../app.js';
+const payOff = document.getElementById('payoff-amount');
+const originalPayment = document.getElementById('option-frequency');
+const cpi = document.getElementById('cpi-amount');
+const originalFrequency = document.getElementById('option-frequency');
+const lateCharge = document.getElementById('late-charge');
+
+const botonSubmit = document.getElementById('boton-submit');
+
+botonSubmit.addEventListener('click', (e)=>{
+    e.preventDefault();
+    console.log(payOff.value);
+    console.log(originalPayment.value);
+console.log(cpi.value);
+console.log(originalFrequency.value);
+console.log(lateCharge.value);
+
+});
+
+
+const cpiAmount = (payOff , cpiAmount) =>{
+    let cpiA = 0;
+    if (payOff.value >= 6000 && originalFrequency.value === 'monthly') {
+        cpiA = 86.67;
+    }
+    if (payOff.value >= 6000 && originalFrequency.value === 'semi-monthly'){
+        cpiA = 86.67;
+    }
+    if (payOff.value >= 6000 && originalFrequency.value === 'semi-monthly'){
+        
+    }
+    
+}
